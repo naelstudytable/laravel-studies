9ca0b6bc4034ca7e4b11f4c78b713bcc219f8892
Natanael A. de Morais <natanael.tocourses@gmail.com>
2023-09-30 09:42:50 -0300

    EP18: Offline States

--- resources/views/livewire/users-list.blade.php
+++ resources/views/livewire/users-list.blade.php
@@ -2,17 +2,31 @@
 {{-- wire:poll.visible: when you want this component to be polled only when it's visible on the screen. --}}
 <div wire:poll.10s>
     <div class="p-6">
+
+        <div wire:offline>
+            <div class="flex items-center p-4 mb-4 text-sm text-yellow-800 rounded-lg bg-yellow-50"
+                role="alert">
+                <svg class="flex-shrink-0 inline w-4 h-4 mr-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor" viewBox="0 0 20 20">
+                    <path
+                        d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+                </svg>
+                <span class="sr-only">Info</span>
+                <div>
+                    <span class="font-medium">Warning alert!</span> You're offline.
+                </div>
+            </div>
+        </div>
+
         <div class="flex justify-between items-center w-full mb-4 gap-2">
             <div class="w-full">
                 {{-- wire:model.live: whenever a user types, it makes requests to synchronize the value in the input field with your property in the backend --}}
                 {{-- wire:model.blur: a request is sent the focus leaves the input --}}
                 {{-- wire:model.debounce.Xms: a network request will only be sent if the user stops typing for at least Xms --}}
                 {{-- wire:model.throttle.Xms: as a user is typing continuously in the "title" field, a network request will be sent every Xms until the user is finished --}}
-                <input
-                wire:model.live='search'
-                type="search"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                placeholder="Search...">
+                <input wire:offline.remove wire:model.live='search' type="search"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                    placeholder="Search...">
             </div>
             {{-- <div>
                 <button
@@ -47,7 +61,8 @@
                         <tr class="bg-white" wire:key='{{ $user->id }}'>
                             <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
                                 <div>
-                                    <img class="w-10 h-10 rounded-lg" src="{{ $user->photo }}" alt="{{$user->name}}">
+                                    <img class="w-10 h-10 rounded-lg" src="{{ $user->photo }}"
+                                        alt="{{ $user->name }}">
                                 </div>
                             </th>
                             <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
@@ -60,7 +75,7 @@
                                 {{ $user->created_at }}
                             </td>
                             <td class="px-6 py-4">
-                                <div wire:click='viewUser({{$user}})'>
+                                <div wire:offline.attr='disabled' wire:click='viewUser({{ $user }})'>
                                     <x-button text="View"/>
                                 </div>
                             </td>
@@ -78,7 +93,7 @@
         <x-slot:body>
             @if ($selectedUser)
                 @foreach ($selectedUser->toArray() as $key => $value)
-                    <p><strong>{{$key}}: </strong>{{$value}}</p>
+                    <p><strong>{{ $key }}: </strong>{{ $value }}</p>
                 @endforeach
             @endif
         </x-slot>

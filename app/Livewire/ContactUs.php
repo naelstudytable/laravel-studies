73c0c66c6b0526304b096c2ac241143958c982d5
Natanael A. de Morais <natanael.tocourses@gmail.com>
2023-11-12 21:45:29 -0300

    EP19: Multi File Upload

--- app/Livewire/ContactUs.php
+++ app/Livewire/ContactUs.php
@@ -3,21 +3,37 @@
 namespace App\Livewire;
 
 use App\Livewire\Forms\ContactUsForm;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class ContactUs extends Component
 {
+    use WithFileUploads;
+
     public ContactUsForm $form;
+
+    #[Rule('nullable|array')]
+    #[Rule(['images.*' => 'image|max:2048'])]
+    public $images;
 
     public function submitForm()
     {
+        $this->validate();
         $this->form->validate();
+
+        if(is_array($this->images)) {
+            foreach ($this->images as $image) {
+                $image->store('images', 'public');
+            }
+        }
 
         // sending email
         $this->form->sendEmail();
 
         session()->flash('success', 'Form submited!');
 
+        $this->reset('images');
         $this->form->reset();
     }
 
